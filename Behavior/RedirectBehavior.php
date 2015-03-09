8d9ee5a0eabd979ede8298fb3afee6d30842895e
Якимов Сергей <s.yakimov@artsofte.ru>
2015-03-09 16:42:36 +0500

    RedirectBehavior

---
+++ Behavior/RedirectBehavior.php
@@ -0,0 +1,273 @@
+<?php
+
+namespace ItBlaster\RedirectBundle\Behavior;
+
+/**
+ * Редиректы
+ *
+ * Class RedirectBehavior
+ * @package ItBlaster\RedirectBundle\Behavior
+ */
+class RedirectBehavior extends \Behavior
+{
+    protected $i18n = NULL;
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function modifyTable()
+    {
+        $this->i18n = $this->getParameter('i18n') == 'true';
+    }
+
+    /**
+     * Аттрибуты
+     */
+    public function objectAttributes()
+    {
+        $table_name = $this->getTable()->getName().($this->i18n ? '' : '_i18n');
+        $attributes = '
+protected $redirect_class_alias = "' . $table_name . '"; //название класса в венгерском стиле
+protected $redirect_object = NULL; //объект редиректа
+protected $redirect; //название класса в венгерском стиле';
+        return $attributes;
+    }
+
+    /**
+     * Добавляем методы в модель
+     *
+     * @param $builder
+     * @return string
+     */
+    public function objectMethods($builder)
+    {
+        $this->builder = $builder;
+        $script = '';
+
+        $this->addGetRedirectClassAlias($script);
+
+        if ($this->i18n) {
+            $this->methodGetRedirect($script);
+            $this->addGetRedirectObject($script);
+            $this->addFindRedirectObject($script);
+            $this->addSetRedirect($script);
+            $this->addSaveRedirect($script);
+        } else {
+            $this->addDeleteRedirects($script);
+        }
+
+        return $script;
+    }
+
+    /**
+     * Алиас класса
+     *
+     * @param $script
+     */
+    protected function addGetRedirectClassAlias(&$script)
+    {
+        $script .= '
+/**
+ * Алиас класса
+ *
+ * @return string
+ */
+public function getRedirectClassAlias()
+{
+    return $this->redirect_class_alias;
+}
+    ';
+    }
+
+    /**
+     * Возвращает старый адрес
+     *
+     * @param $script
+     */
+    protected function methodGetRedirect(&$script)
+    {
+        $script .= '
+/**
+ * Возвращает старый адрес
+ *
+ * @return string
+ */
+public function getRedirect()
+{
+    return $this->getRedirectObject() ? $this->getRedirectObject()->getOldUrl() : "";
+}
+    ';
+    }
+
+    /**
+     * Устанавливаем старый адрес
+     *
+     * @param $script
+     */
+    protected function addSetRedirect(&$script)
+    {
+        $script .= '
+/**
+ * Устанавливаем старый адрес
+ *
+ * @return string
+ */
+public function setRedirect($value)
+{
+    $this->redirect = $value;
+    $redirect_object = $this->getRedirectObject();
+    if ($value && !$redirect_object) {
+        $redirect_object = $this->findRedirectObject();
+    }
+    if ($redirect_object && ($redirect_object->getOldUrl()!=$value || !$value)) {
+        $this->modifiedColumns[] = "redirect";
+    }
+}
+    ';
+    }
+
+    /**
+     * Сохраняем редирект
+     *
+     * @param $script
+     */
+    protected function addSaveRedirect(&$script)
+    {
+        $script .= '
+/**
+ * Сохраняем редирект
+ */
+public function saveRedirect()
+{
+    if ($this->redirect) {
+        $redirect_object = $this->findRedirectObject();
+        $redirect_object
+            ->setLocale($this->getLocale())
+            ->setOldUrl($this->redirect)
+            ->save();
+    } /* else {
+        $redirect_object = $this->getRedirectObject();
+        if ($redirect_object) {
+            $redirect_object->delete();
+        }
+    } */
+}
+    ';
+    }
+
+    /**
+     * Возвращает объект редиректа
+     * Если объекта редиректа нет, создаёт его
+     *
+     * @param $script
+     */
+    protected function addFindRedirectObject(&$script)
+    {
+        $script .= '
+/**
+ * Возвращает объект редиректа
+ * Если объекта редиректа нет, создаёт его
+ *
+ * @return string
+ */
+public function findRedirectObject()
+{
+    $redirect_object = $this->getRedirectObject();
+    if (!$redirect_object && $this->getId()) {
+        $redirect_object = new \ItBlaster\RedirectBundle\Model\Redirect();
+        $redirect_object
+            ->setModel($this->getRedirectClassAlias())
+            ->setLocale($this->getLocale())
+            ->setObjectId($this->getId())
+            ->save();
+        $this->redirect_object = $redirect_object;
+    }
+    return $redirect_object;
+}
+    ';
+    }
+
+    /**
+     * Объект редиректа
+     *
+     * @param $script
+     */
+    protected function addGetRedirectObject(&$script)
+    {
+        $script .= '
+/**
+ * Объект редиректа
+ *
+ * @return string
+ */
+public function getRedirectObject()
+{
+    if ($this->redirect_object===NULL) {
+        $this->redirect_object = \ItBlaster\RedirectBundle\Model\RedirectQuery::create()
+            ->filterByModel($this->getRedirectClassAlias())
+            ->filterByLocale($this->getLocale())
+            ->filterByObjectId($this->getId())
+            ->findOne();
+    }
+    return $this->redirect_object;
+}
+    ';
+    }
+
+    /**
+     * Метод сохранения редиректов в postSave
+     *
+     * @param $builder
+     * @return string
+     */
+    public function postSave($builder)
+    {
+        $this->builder = $builder;
+        $script = '';
+        if ($this->i18n) {
+            $script .= "\$this->saveRedirect(); //После сохранения объекта сохраняем редиректы";
+        }
+        return $script;
+    }
+
+    /**
+     * Удаление связанных объектов редиректов
+     *
+     * @param $script
+     */
+    public function addDeleteRedirects(&$script)
+    {
+        $script .= '
+/**
+ * Удаление связанных объектов редиректов
+ *
+ * @return boolean
+ */
+public function deleteRedirects()
+{
+    return BaseRedirectQuery::create()
+        ->filterByModel($this->getRedirectClassAlias())
+        ->filterByObjectId($this->getId())
+        ->delete();
+}
+        ';
+    }
+
+    /**
+     * Удаляем связанные объекты редиректов
+     * перед удалением текущего объекта
+     *
+     * @param $builder
+     * @return string
+     */
+    public function preDelete($builder)
+    {
+        $this->builder = $builder;
+        $script = '';
+        if (!$this->i18n) {
+            $script .= "
+\$this->deleteRedirects(); //Перед удалением объекта удаляем прикреплённые объекты редиректов";
+        }
+        return $script;
+    }
+}
--- README.md
+++ README.md
@@ -41,6 +41,50 @@ public function registerBundles()
 }
 ```
 
+Use redirect in models
+-------
+С помощью <b>RedirectBundle</b> редиректы можно добавлять в формы редактирования сущностей. Для этого необходимо:
+1. указать в файле `config.yml` параметр `it_blaster_redirect.use_model`
+``` bash
+it_blaster_redirect:
+    locales: ['ru', 'en','uk','cs']
+    use_model: true
+```
+2. подключить бихейвор `RedirectBehavior` в `config.yml`
+``` bash
+ propel:
+     ...
+     behaviors:
+         ...
+         it_blaster_redirect: ItBlaster\RedirectBundle\Behavior\RedirectBehavior
+```
+3. в файле `schema.yml` добавить бихейвор `it_blaster_redirect` к описанию молели:
+``` bash
+    <table name="news" description="Новости">
+        <column name="id"                   type="integer"  required="true" primaryKey="true" autoIncrement="true" />
+        <column name="title"                type="varchar"  required="true" primaryString="true" />
+        <column name="alias"                type="varchar"  required="true" />
+        <column name="date"                 type="date"     required="true" />
+        <column name="short_desc"           type="longvarchar" />
+        <column name="full_desc"            type="longvarchar" />
+        <column name="active"               type="boolean" />
+
+        <behavior name="i18n">
+            <parameter name="i18n_columns" value="title, short_desc, full_desc" />
+        </behavior>
+
+        <behavior name="it_blaster_redirect" >
+            <parameter name="i18n" value="false" />
+        </behavior>
+    </table>
+
+    <table name="news_i18n" description="Новости. Языковые версии">
+        <behavior name="it_blaster_redirect" >
+            <parameter name="i18n" value="true" />
+        </behavior>
+    </table>
+```
+
 Credits
 -------
 
